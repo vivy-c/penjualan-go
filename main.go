b9604ac7ef0a92c6f3d3689fca1821f06585032a
Vivy Cahyani <cahyanivivy@gmail.com>
2021-12-22 17:32:10 +0700

    membuat function input array

--- main.go
+++ main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	
+	size := 0  
+	fmt.Print("n+m=")  
+	fmt.Scanln(&size)  
+	fmt.Println("isi elemen")  
+	elements := make([]int, size)  
+	for i := 0; i < size; i++ {  
+	 fmt.Scanln(&elements[i])  
+	}  
+	fmt.Println("elemen yang diinput:", elements)  
+	result := 0  
+	 
+	for i := 0; i < size; i++ {  
+	 result += elements[i]  
+	 
+	}  
+	fmt.Println("hasil:", result)  
 }
