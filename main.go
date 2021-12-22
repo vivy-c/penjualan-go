8ea13d5f1ad1bca7250aea259aced5a762b674a3
Vivy Cahyani <cahyanivivy@gmail.com>
2021-12-22 18:35:50 +0700

    menambah keterangan

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ func main() {
 	
 	n := 0
 	m := 0
-	fmt.Print("n=")  
+	fmt.Print("jumlah terminal (n)=")  
 	fmt.Scanln(&n)  
-	fmt.Print("m=")  
+	fmt.Print("jumlah toko (m)=")  
 	fmt.Scanln(&m) 
 	fmt.Print("n+m=")  
 	size := n+m
 	fmt.Println(size)  
-	fmt.Println("isi elemen")  
+	fmt.Println("isi elemen (enter sebagai pemisah) :")  
 	elements := make([]int, size)  
 	for i := 0; i < size; i++ {  
 	 fmt.Scanln(&elements[i])  
